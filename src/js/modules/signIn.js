5539da134784d897642067ba55ddc08e04d2f9fa
NiakaGaming <niaka-ishigame@hotmail.com>
2020-10-07 10:02:47 +0200

    SignIn/Up done

--- src/js/modules/signIn.js
+++ src/js/modules/signIn.js
@@ -1,23 +1,47 @@
 let signInForm = document.getElementById("signIn")
 let btnSignIn = document.getElementById("connexion");
-let btnsignUp = document.getElementById("signUp");
+let btnSignUp = document.getElementById("signUpBtn");
 let eyeShow = document.getElementById("eyeShow");
 let eyeHide = document.getElementById("eyeHide");;
 let mdpInput = eyeShow.previousElementSibling;
 
-// document.getElementsByTagName("body")[0].addEventListener("click", () => {
-//     signInForm.toggleAttribute("hidden", true);
-// })
+let signUpForm = document.getElementById("signUp")
+let btnSignUp2 = document.getElementById("signInBtn");
 
+let cross1 = signInForm.firstElementChild.firstElementChild.firstElementChild;
+let cross2 = signUpForm.firstElementChild.firstElementChild.firstElementChild;
+
+cross1.addEventListener("click", exit);
+cross2.addEventListener("click", exit);
+function exit() {
+    signInForm.toggleAttribute("hidden", true);
+    signUpForm.toggleAttribute("hidden", true);
+}
+document.addEventListener("keydown", keyExit);
+function keyExit(e) {
+    if (e.key == "Escape") {
+        exit();
+    }
+}
+
+let showSignIn = false;
+let showSingUp = false;
 // Connexion
 btnSignIn.addEventListener("click", signIn);
 function signIn() {
     signInForm.toggleAttribute("hidden", false);
 }
-// Inscription
-btnsignUp.addEventListener("click", signUp);
+// Inscription show
+btnSignUp.addEventListener("click", signUp);
 function signUp() {
     signInForm.toggleAttribute("hidden", true);
+    signUpForm.toggleAttribute("hidden", false);
+}
+// Connexion show
+btnSignUp2.addEventListener("click", toSignIn);
+function toSignIn() {
+    signUpForm.toggleAttribute("hidden", true);
+    signInForm.toggleAttribute("hidden", false);
 }
 
 // hide/show icon
